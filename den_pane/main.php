57dd57efa056407e8e799e5e021824d0ccde84dc
Jaromir Feilhauer <j.feilhauer@email.cz>
2021-03-20 20:02:01 +0100

    add admin interface

--- den_pane/main.php
+++ den_pane/main.php
@@ -9,6 +9,7 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
 include 'den_pane.php';
+include 'admin.php';
 // vytvoření kódu pro vložení do stránky [/den_pane_zkouska]
 // pro wordpress
 if( !is_admin() ) add_shortcode("den_pane_zkouska", 'den_pane');
